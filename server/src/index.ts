b121070c1e9ced6235f8e687741ddf48c55e5044
Y.Rama <114061024+YRama2024@users.noreply.github.com>
2024-01-05 16:35:15 +0530

    Updated  index.ts for vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,13 @@ import { productRouter } from "./routes/product";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST","GET"],
+    credentials:true
+  }
+));
 
 app.use("/auth", userRouter);
 app.use("/products", productRouter);
